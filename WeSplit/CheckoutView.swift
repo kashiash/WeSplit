e1e28cc00b519361710a4d2216a3a8dcc3f4f94e
kashiash <kashiash@gmail.com>
2022-06-18 21:11:23 +0200

    checkout view cd

--- WeSplit/CheckoutView.swift
+++ WeSplit/CheckoutView.swift
@@ -10,14 +10,48 @@ import SwiftUI
 struct CheckoutView: View {
     @EnvironmentObject var order: Order
     @State private var paymentType = "Cash"
+    @State private var addLoyaltyDetails = false
+    @State private var loyaltyNumber = ""
+    @State private var tipAmount = 15
+    let tipAmounts = [10, 15, 20, 25, 0]
     
-    let paymentTypes = ["Cash","Credit Card","PysznePl Points"]
+    
+    var totalPrice: String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+
+        let total = Double(order.total)
+        let tipValue = total / 100 * Double(tipAmount)
+
+        return formatter.string(from: NSNumber(value: total + tipValue)) ?? "$0"
+    }
+    
+    let paymentTypes = ["Cash","Credit Card","Żryj z hasioka Points"]
     var body: some View {
-        VStack {
+        Form {
             Section {
                 Picker("How do you want to pay?", selection: $paymentType) {
                     ForEach(paymentTypes, id: \.self) {
                         Text($0)
+                    }
+                }
+                Toggle("Add Żryj z hasioka loyalty card", isOn: $addLoyaltyDetails.animation())
+                if addLoyaltyDetails {
+                    TextField("Enter your Żryj z hasioka ID", text: $loyaltyNumber)
+                }
+                Section(header: Text("Add a tip?")) {
+                    Picker("Percentage:", selection: $tipAmount) {
+                        ForEach(tipAmounts, id: \.self) {
+                            Text("\($0)%")
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                Section(header:
+                    Text("TOTAL: \(totalPrice)")
+                ) {
+                    Button("Confirm order") {
+                        // place the order
                     }
                 }
             }
